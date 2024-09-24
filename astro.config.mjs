675ccfbd3a378d50d74db2a9574fefc44707335c
saugatnp <saugatnp11@gmail.com>
2024-09-24 15:47:40 +0545

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from 'astro/config';
+import icon from "astro-icon";
+
+
+// https://astro.build/config
+export default defineConfig({
+    integrations: [icon({
+        include: {
+          mdi: ["*"], 
+          mdi: ["account"],
+        },
+      }),],
+});
